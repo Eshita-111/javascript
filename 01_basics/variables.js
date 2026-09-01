62dd0522867a14e38575508da5b53a60c549e6ec
Sumaiya Nasir <sumaiyanasir95@gmail.com>
2026-09-01 22:15:58 +0600

    variables done.

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 14223
+let accountEmail = "sh@google.com"
+var accountPassword = "31556"
+accountCity = "Dhaka"
+let accountState;
+
+accountEmail = "sumaiya@gmail.com"
+accountPassword = "7333"
+accountCity = "Barishal"
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountState, accountCity])
